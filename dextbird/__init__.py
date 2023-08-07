05ca2483235161f6fe84a4a7cfee6d51a8bb2ad1
mc_fdc <Masato.04.11.2007@gmail.com>
2023-08-07 15:35:38 +0900

    __init__.py を更新

--- dextbird/__init__.py
+++ dextbird/__init__.py
@@ -5,7 +5,14 @@ from .dextbird import Core
 class VoiceClient(discord.VoiceProtocol):
 
     def __init__(self, client, channel):
+        self.channel = channel
+        self.guild = channel.guild
         self._core = Core(client, channel.guild.id, client.user.id)
+        super().__init__()
 
-    async def connect(self, **kwargs):
-        await self._core.join()
+    async def connect(self, *, self_deaf=False, self_mute=False):
+        await self.guild.change_voice_state(
+            self.channel,
+            self_deaf=self_deaf,
+            self_mute=self_mute,
+        )
