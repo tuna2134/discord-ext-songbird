5271e4d7ccafa6f7a988c08f211718a3f90ddd28
mc_fdc <Masato.04.11.2007@gmail.com>
2023-08-07 18:16:23 +0900

    core.rs を更新

--- src/core.rs
+++ src/core.rs
@@ -18,7 +18,7 @@ impl Core {
     pub fn new(py: Python<'_>, client: Py<PyAny>, guild_id: u64, user_id: u64) -> Self {
         let rt = Builder::new_multi_thread()
             .thread_name("dextbird-core");
-        pyo3_asyncio::tokio::init(rt);
+        pyo3_asyncio::tokio::init(&rt);
         let shard = Shard::Generic(Arc::new(VoiceUpdate {
             client: client.as_ref(py).clone().into(),
         }));
