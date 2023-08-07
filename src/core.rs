ad06fd7ee16aaa297623a7f8a36f607e8dac61de
mc_fdc <Masato.04.11.2007@gmail.com>
2023-08-07 05:01:56 +0000

    test: 💍 ogkskc

--- src/core.rs
+++ src/core.rs
@@ -4,10 +4,11 @@ use songbird::Call;
 use songbird::id::{ChannelId, GuildId, UserId};
 use pyo3::prelude::*;
 use std::sync::Arc;
+use tokio::sync::Mutex;
 
 #[pyclass]
 struct Core {
-    call: Call,
+    call: Arc<Mutex<Call>>,
 }
 
 #[pymethods]
@@ -16,12 +17,13 @@ impl Core {
     fn new(py: Python<'_>, client: Py<PyAny>, guild_id: u64, user_id: u64) -> Self {
         let shard = Shard::Generic(Arc::new(VoiceUpdate { client: client.as_ref(py).clone().into() } ));
         let call = Call::new(GuildId(guild_id), shard, UserId(user_id));
-        Self { call }
+        Self { call: Arc::new(Mutex::new(call)) }
     }
      
     fn join(&self, py: Python, channel_id: u64) -> PyResult<&PyAny> {
-        let mut call = self.call.clone();
+        let mut call = Arc::clone(&self.call);
         pyo3_asyncio::tokio::future_into_py(py, async move {
+            let mut call = call.lock().await;
             call.join(ChannelId(channel_id)).await.unwrap();
             Ok(())
         })
