2f72be6b0030862d19f98b9a5e24429b9a2030b4
mc_fdc <Masato.04.11.2007@gmail.com>
2023-09-06 10:16:15 +0000

    add disable loop

--- src/track.rs
+++ src/track.rs
@@ -20,6 +20,11 @@ impl Track {
         Ok(())
     }
 
+    pub fn disable_loop(&self) -> PyResult<()> {
+        self.handle.disable_loop().unwrap()
+        Ok(())
+    }
+
     pub fn set_volume(&self, volume: f32) -> PyResult<()> {
         self.handle.set_volume(volume).unwrap();
         Ok(())
