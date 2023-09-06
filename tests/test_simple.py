d81c7d82ae58ff170a5fe8a4b3a55331ba9e618f
mc_fdc <Masato.04.11.2007@gmail.com>
2023-09-06 22:05:00 +0900

    Update test_simple.py

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -12,10 +12,17 @@ client = discord.Client(intents=discord.Intents.all())
 async def test_some_asyncio_code():
     @client.event
     async def on_ready() -> None:
+        print("Starting test")
         channel = client.get_channel(961916734523179050)
+        print("Connecting to vc")
         vc = await channel.connect(cls=dextbird.VoiceClient)
+        print("Playing music")
         (await vc.ytdl("https://youtu.be/Vi-1402wYtI?si=x_rhftnpQ0fKcfEE")).play()
+        print("Waiting 15 second")
         await asyncio.sleep(15)
+        print("Disconnect from vc")
+        await vc.disconnect()
+        print("Finishing test")
         await client.close()
 
     await client.start(os.getenv("TOKEN"))
