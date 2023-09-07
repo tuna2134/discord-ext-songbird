dcc90074696decefed0b5a36fdca800b118b5012
mc_fdc <Masato.04.11.2007@gmail.com>
2023-09-07 22:35:19 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,13 @@ mod core;
 mod update_voice_state;
 mod track;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
 /// A Python module implemented in Rust.
 #[pymodule]
 fn dextbird(_py: Python, m: &PyModule) -> PyResult<()> {
     pyo3_log::init();
     
-    m.add_class::<core::Core>()?;
+    m.add_class::<crate::core::Core>()?;
     m.add_class::<track::Track>()?;
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_function(wrap_pyfunction!(core::setup, m)?)?;
+    m.add_function(wrap_pyfunction!(crate::core::setup, m)?)?;
     Ok(())
 }
