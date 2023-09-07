46d159484aee4f0abc84e58607a8247dde12bb13
mc_fdc <Masato.04.11.2007@gmail.com>
2023-09-07 11:26:51 +0900

    Update __init__.py

--- dextbird/__init__.py
+++ dextbird/__init__.py
@@ -6,6 +6,7 @@ from typing import Optional
 
 
 class VoiceClient(discord.VoiceProtocol):
+    "discord.py extensions voiceclient"
     
     def __init__(self, client: discord.Client, channel: discord.abc.Connectable):
         self.channel = channel
@@ -18,6 +19,7 @@ class VoiceClient(discord.VoiceProtocol):
         super().__init__(client, channel)
 
     async def connect(self, *, self_deaf=False, self_mute=False, **kwargs) -> None:
+        "Connect to voice channel"
         self._core = await setup(self.client, self.guild.id, self.client.user.id)
         await self._core.join(self.channel.id)
 
@@ -27,14 +29,17 @@ class VoiceClient(discord.VoiceProtocol):
         self.connected = True
 
     async def on_voice_server_update(self, data: dict) -> None:
+        "Update voice server"
         await self._core.update_server(data["endpoint"], data["token"])
         self.voice_server_event.set()
 
     async def on_voice_state_update(self, data: dict) -> None:
+        "Update voice state"
         await self._core.update_state(data["session_id"], data.get("channel_id"))
         self.voice_state_event.set()
 
-    async def ytdl(self, url) -> Track:
+    async def ytdl(self, url: string) -> Track:
+        "Play music by yt-dlp"
         return await self._core.ytdl(url)
 
     async def source(self, data: bytes) -> Track:
@@ -42,9 +47,11 @@ class VoiceClient(discord.VoiceProtocol):
         return await self._core.source(data)
 
     async def stop(self) -> None:
+        "Stop to play music"
         await self._core.stop()
 
     async def disconnect(self, *args) -> None:
+        "Disconnect from voice channel"
         await self._core.leave()
         self._core = None
         self.connected = False
